89813d009227dcae63519a8f8d978ebbecd97816
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-05-03 14:10:36 +0530

    Implement mobile and crypto payments

--- src/CryptoPayment.java
+++ src/CryptoPayment.java
@@ -1,2 +1,21 @@
-public class CryptoPayment {
+public class CryptoPayment extends Payment{
+
+    private String walletAddress;
+    private String currenncy;
+
+
+
+    public CryptoPayment(double amount, String merchant, String currency,String walletAddress) {
+        super(amount, merchant);
+        this.walletAddress = walletAddress;
+        this.currenncy = currency;
+    }
+
+    @Override
+    void process() {
+
+        System.out.println("Processing crypto payment with " + this.currenncy);
+        System.out.println("For amount of - "+ getAmount());
+
+    }
 }
--- src/MobilePayment.java
+++ src/MobilePayment.java
@@ -1,2 +1,32 @@
-public class MobilePayment {
+public class MobilePayment extends Payment implements Refundable{
+
+    private String mobileNo;
+    private String mobileApp;
+
+
+    public MobilePayment(double amount, String merchant, String mobileNo, String mobileApp) {
+        super(amount, merchant);
+        this.mobileNo = mobileNo;
+        this.mobileApp = mobileApp;
+    }
+
+    @Override
+    void process() {
+
+        System.out.println(" Mobile ");
+
+    }
+
+
+    @Override
+    public void refund() {
+        System.out.println("Refunfding mobile payment "+ getAmount());
+
+    }
+
+    @Override
+    public String getRefundPolicy() {
+
+        return  "Payment will  be refunded only within 14 days";
+    }
 }
